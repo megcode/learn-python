0f0eef41296030adf837bd97bf6476c1fbf0cfbb
megcode <megcode@gmail.com>
2021-01-11 12:16:24 -0800

    I wrote this beatiful program at 12,it uses if else

---
+++ program1.py
@@ -0,0 +1,15 @@
+# My first program
+h = 56
+a = 45
+w = 34
+c = 57.3
+animal = "panda"
+print(c,animal,w,"tiger")
+if (a<w):
+    print("a is smaller than w")
+    print("this line belongs to first if")
+elif (a>w):
+    print("a is bigger")
+    print("this line belons to elif")
+else:
+    print("a is equal to w")
