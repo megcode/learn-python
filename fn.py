b5a1590d0d66b5afe5eadb486fcbdb578160beec
megcode <megnaprakash@protonmail.com>
2021-01-19 11:30:29 -0800

    I wrote this dimunitive function program at 11:10 am

---
+++ fn.py
@@ -0,0 +1,10 @@
+# My first function program
+#define the function, with i as input
+def my_fn(i):
+    c1 = i*5
+    c2 = c1-5
+    c3 = c2/5
+    print(c3)
+
+# call the function, send a value to i
+my_fn(100)
