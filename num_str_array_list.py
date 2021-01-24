423afb1c85ee9cca43e4fac6cb5372f99a1053d4
megcode <megnaprakash@protonmail.com>
2021-01-24 11:01:54 -0800

    This is about array list, and built in functions

---
+++ num_str_array_list.py
@@ -0,0 +1,14 @@
+b = [6,7,8,87,192,205,666,36,9,2,1,6.5]
+print(b[0])
+print(b[0]+b[1])
+asc = sorted(b)
+print(asc)
+desc = sorted(b,reverse=True)
+print(desc)
+dogs = ["golden","maltipoo","maltese"]
+print(dogs[1])
+s = "flower"
+caps = s.capitalize()
+caps1 = s.upper()
+print(caps)
+print(caps1)
